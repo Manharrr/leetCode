28baa043e78a715e7b758edbf8b6b38d916b3d90
Manharrr <manhargurukkal2@gmail.com>
2026-02-25 08:02:20 +0530

    Time: 0 ms (100%), Space: 12.5 MB (56.74%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        import math
+        return int(math.sqrt(x))
+
+
+
+       
+        
+        
